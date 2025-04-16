ced3d7e1f33fa8d43e0f540932078d5fd440803c
LiangTsao <LiangTsao@qq.com>
2025-04-16 16:49:17 +0800

    s

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import com.example.core.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from demo-java-single-module!");
+        System.out.println(StringUtils.reverse("123456789"));
     }
 }
