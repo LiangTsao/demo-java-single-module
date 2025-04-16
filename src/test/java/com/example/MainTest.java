fce07e6366725a94b58e9e208560e4a6839895ce
LiangTsao <LiangTsao@qq.com>
2025-04-16 17:12:56 +0800

    xx

--- src/test/java/com/example/MainTest.java
+++ src/test/java/com/example/MainTest.java
@@ -3,23 +3,27 @@ package com.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import com.example.core.StringUtils;
+
 public class MainTest {
 
     @Test
     public void testStringReverse() {
-        // Test normal string reversal
-        assertEquals("987654321", com.example.core.StringUtils.reverse("123456789"));
-        
-        // Test empty string
-        assertEquals("", com.example.core.StringUtils.reverse(""));
-        
-        // Test single character
-        assertEquals("a", com.example.core.StringUtils.reverse("a"));
-        
-        // Test null input
-        assertNull(com.example.core.StringUtils.reverse(null));
-        
-        // Test string with spaces
-        assertEquals("cba ", com.example.core.StringUtils.reverse(" abc"));
+        assertEquals("987654321", StringUtils.reverse("123456789"));
+    }
+
+    @Test
+    public void testEmptyString() {
+        assertEquals("", StringUtils.reverse(""));
+    }
+
+    @Test
+    public void testNullString() {
+        assertNull(StringUtils.reverse(null));
+    }
+
+    @Test
+    public void testSpecialCharacters() {
+        assertEquals("!@#$%", StringUtils.reverse("%$#@!"));
     }
 }
